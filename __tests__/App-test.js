24c0b17ac5618f879c23a5c27b09816dbbd6bf61
Animesh <animeshk55@gmail.com>
2021-04-28 14:41:46 +0530

    working test cases

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,31 @@
-/**
- * @format
- */
+// /**
+//  * @format
+//  */
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+// import 'react-native';
+// import React from 'react';
+// import App from '../App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+// // Note: test renderer must be required after react-native.
+// import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+// it('renders correctly', () => {
+//   renderer.create(<App />);
+// });
+
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "../App.js";
+
+describe("<App />", () => {
+  it('has 2 child', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree.children.length).toBe(2);
+  })
+
+  it('renders correctly across screens', () => {
+    const tree = renderer.create().toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+})
+
